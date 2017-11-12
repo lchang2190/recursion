3af4d92ff3f3eb3353069cfbebc6173676e495da
Larry <lchang2190@gmail.com>
2017-11-12 15:48:41 -0800

    Finished Recursion Assignment

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+	var results = [];
+
+	// set node to node, or document.body as default;
+	node = node || document.body;
+
+	//check node classes for className, if it does, push node into results;
+	if ( node.classList.contains(className)){
+		results.push(node);
+	}
+
+	//check child nodes for className, if it exits add to results;
+	for ( var i = 0; i < node.children.length; i++ ){
+		var child = getElementsByClassName(className, node.children[i]);
+		results = results.concat(child);
+	}
+
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,60 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //null
+  if (obj === null){
+    return 'null';
+  }
+
+  //unstringifiable
+  if ( obj === undefined || typeof obj === Function){
+    return ;
+  }
+
+  //strings
+  if (typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+
+  //arrays
+  if (Array.isArray(obj)){
+    if (obj.length <= 0){
+      return '[]';
+    }
+
+    var array = '[';
+    for( var i = 0; i < obj.length; i++ ){
+      if (i !== obj.length - 1){
+        array += stringifyJSON(obj[i]) + ',';
+      }
+      else array += stringifyJSON(obj[i]) + ']';
+    }
+    return array;
+  }
+
+  //objects
+  if ( typeof obj === 'object'){
+    if (obj.length <= 0){
+      return '{}';
+    }
+
+    var keys = Object.keys(obj);
+    var object = '{';
+    for (var i = 0; i < keys.length; i++){
+      if (!keys[i] || obj[keys[i]] === undefined || typeof keys[i] === 'function' || typeof obj[keys[i]] === 'function') {
+
+      }else{
+        if (i !== keys.length - 1){
+          object += stringifyJSON(keys[i]) + ':' + stringifyJSON(obj[keys[i]]) + ',';    
+        }
+        else object += stringifyJSON(keys[i]) + ':' + stringifyJSON(obj[keys[i]]);
+        }
+      }
+
+    return object + '}';
+  }
+  return obj.toString();
+  
 };
+
+
